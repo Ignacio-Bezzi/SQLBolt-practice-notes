0cb4bf957486d66527e712576eac77a9c35f7172
Ignacio-Bezzi <ignaciobezzi.work@gmail.com>
2026-08-25 19:37:29 -0300

    Create SQL_Lesson_1.sql with SELECT query notes
    Added introductory comments for SQL lesson on SELECT queries.

---
+++ SQL_Lesson_1.sql
@@ -0,0 +1,16 @@
+/* =========================================
+   LESSON 1: SELECT queries 101
+   TOPIC: SELECT
+============================================
+NOTES:
+   - Query: A statement that declares what data we want and where to find it.
+   - Core Concepts: 
+     > Table = Entity (e.g., Dogs)
+     > Row = Specific instance (e.g., A pug)
+     > Column = Shared property (e.g., Fur color)
+     
+   SYNTAX:
+   - SELECT col1, col2 FROM table; -> Retrieves specific columns.
+   - SELECT * FROM table; -> Retrieves ALL columns (useful for a quick inspection).
+========================================= */
+
